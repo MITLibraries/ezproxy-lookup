1da6446544e9662c5d21e018b99d58f755af6b6b
Adam Shire <shirea@mit.edu>
2019-11-01 13:02:12 -0400

    don't use s3 if flask is in development mode

--- ezproxylookup/helpers.py
+++ ezproxylookup/helpers.py
@@ -1,12 +1,17 @@
 import boto3
 import os
 import json
+from ezproxylookup import app
 
 
 # get config.json from s3 bucket.
 def get_file():
-    s3 = boto3.resource('s3')
-    obj = s3.Object(os.getenv('AWS_BUCKET_NAME'), 'config.json')
-    body = obj.get()['Body'].read()
-    data = json.loads(body)
+    if app.config['ENV'] == 'development':
+        with open(os.getenv('DEV_CONFIG_JSON')) as f:
+            data = json.loads(f.read())
+    else:
+        s3 = boto3.resource('s3')
+        obj = s3.Object(os.getenv('AWS_BUCKET_NAME'), 'config.json')
+        body = obj.get()['Body'].read()
+        data = json.loads(body)
     return data
